896e534a04b6eb6b445d6dffb03e1f4ce5270d4d
kamen-kamen <ne.kamenvprostracii@gmail.com>
2026-06-29 20:32:05 +0300

    fix: .env file
    add dotend dependency

--- src/main/java/com/waregang/receiving_service/ReceivingServiceApplication.java
+++ src/main/java/com/waregang/receiving_service/ReceivingServiceApplication.java
@@ -1,12 +1,19 @@
 package com.waregang.receiving_service;
 
+import com.waregang.receiving_service.security.configuration.JwtProperties;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-//@EnableConfigurationProperties(JwtProperties.class)
+@EnableConfigurationProperties(JwtProperties.class)
 public class ReceivingServiceApplication {
-	public static void main(String[] args) {
+	static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+
 		SpringApplication.run(ReceivingServiceApplication.class, args);
 	}
 
